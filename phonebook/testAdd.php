0f238a3ee616c3d3177cfbdaf872dd9a3e864c72
indra-suryatama <indra_suryatama@hotmail.com>
2022-10-29 16:25:54 +0000

    test

--- phonebook/testAdd.php
+++ phonebook/testAdd.php
@@ -10,40 +10,16 @@
 		$id = $result[0] + 1;
 	}
 	echo('The ID is'.$id);
-	//$stid = oci_parse($conn, $sql);
-	//oci_execute($stid);
+
     $loginID = 1;
     $name = 'Keiko';
     $address = 'tomang';
     $phoneNumber = '628595';
 	
 	$sql2 = 'INSERT INTO phonebook VALUES('.$id.', \''.$name.'\', \''.$address.'\', \''.$phoneNumber.'\', '.$loginID.')';
-    
-   // $sql = 'SELECT id FROM login WHERE name= \''.$username.'\' and password =\''.$password.'\'';
-   //insert into phonebook values (3,'levana','jln mandala aa,','08555555',1);
-
-	//echo($sql2);
-	$row = $myPDO->prepare($sql2);
+    $row = $myPDO->prepare($sql2);
 	$row->execute();
     echo ('success add');
-	/*
-	$stid = oci_parse($conn, 'INSERT INTO phonebook (id, loginId, name, address, phoneNumber) VALUES(:id, :loginid, :myname, :myaddress, :myphonenumber)');
-
-	oci_bind_by_name($stid, ':id', $id);
-	oci_bind_by_name($stid, ':loginid', $loginID);
+    //Berhasil
 	
-	oci_bind_by_name($stid, ':myname', $name);
-	oci_bind_by_name($stid, ':myaddress', $address);
-	oci_bind_by_name($stid, ':myphonenumber', $phoneNumber);
-
-	$r = oci_execute($stid);  // executes and commits
-
-	if ($r) {
-		print "One row inserted";
-	}
-
-	oci_free_statement($stid);
-	oci_close($conn);*/
-	//header("HTTP/1.1 200 OK");
-	//var_dump($data);
 ?>
--- phonebook/update.php
+++ phonebook/update.php
@@ -7,27 +7,11 @@
 	$name = $_POST["name"];
 	$address = $_POST["address"];
 	$phoneNumber = $_POST["phoneNumber"];
-	
-	echo $id.' '.$name;
-	$conn = oci_connect('system', 'abcd1234', 'localhost/XE');
-	
-	
-	$stid = oci_parse($conn, 'UPDATE phonebook set name=:myname, address=:myaddress, phoneNumber=:myphonenumber WHERE id = :id') ;
-	// $query = oci_parse($conn, "UPDATE employee SET userid='" . $_POST['userid'] . "', first_name='" . $_POST['first_name'] . "', last_name='" . $_POST['last_name'] . "', city_name='" . $_POST['city_name'] . "' ,email='" . $_POST['email'] . "' WHERE userid='" . $_POST['userid'] . "'");
-	oci_bind_by_name($stid, ':id', $id);
-	oci_bind_by_name($stid, ':myname', $name);
-	oci_bind_by_name($stid, ':myaddress', $address);
-	oci_bind_by_name($stid, ':myphonenumber', $phoneNumber);
-   
-    //var_dump($stid);
-	$r = oci_execute($stid);  // executes and commits
 
-	if ($r) {
-		print "One row updated";
-	}
 
-	oci_free_statement($stid);
-	oci_close($conn);
-	//header("HTTP/1.1 200 OK");
-	//var_dump($data);
+	$myPDO = new PDO('pgsql:host=10.1.137.140;dbname=testdb','postgres','abcd1234');
+	$sql= 'UPDATE phonebook set name=\''.$name.'\', address=\''.$address.'\', phoneNumber=\''.$phoneNumber.'\', WHERE id = '.$id;
+	//$sql2 = 'INSERT INTO phonebook VALUES('.$id.', \''.$name.'\', \''.$address.'\', \''.$phoneNumber.'\', '.$loginID.')';
+	$row= $myPDO->prepare($sql);
+	$row->execute();
 ?>
