8edd4f52e242454ba1449909a04a53a81dca5718
indra-suryatama <indra_suryatama@hotmail.com>
2022-10-29 16:01:17 +0000

    Update getPhonebook.php

--- phonebook/getPhonebook.php
+++ phonebook/getPhonebook.php
@@ -1,14 +1,13 @@
 <?php
 	ob_start();
     $id = $_POST['id']; 
-    $conn = oci_connect('system', 'abcd1234', 'localhost/XE');
-	
+    $myPDO = new PDO('pgsql:host=10.1.137.140;dbname=testdb','postgres','abcd1234');
 	$sql = 'SELECT * FROM phonebook WHERE id = '.$id;
 	
-	$stid = oci_parse($conn, $sql);
-	oci_execute($stid);
+	$result = $myPDO->prepare($sql);
+	$result->execute();
 	
-	if($row = oci_fetch_row($stid)) {
+	if($row=$result->fetch()) {
         $data = array("id"=> $row[0], "name"=> $row[1], "address"=>  $row[2], "phoneNumber" => $row[3]);
         echo json_encode($data);
 	}
