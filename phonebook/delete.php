171429e2b1c95b3977c7529d0ed9bd7f994329f3
indra-suryatama <indra_suryatama@hotmail.com>
2022-10-29 16:20:49 +0000

    Update delete.php

--- phonebook/delete.php
+++ phonebook/delete.php
@@ -3,7 +3,7 @@
 	session_start();
 	$id = $_POST["id"];
 	$myPDO = new PDO('pgsql:host=10.1.137.140;dbname=testdb','postgres','abcd1234');
-	$sql= 'DELETE phonebook WHERE id ='.$id;
+	$sql= 'DELETE FROM phonebook WHERE id ='.$id;
 	$row= $myPDO->prepare($sql);
 	$row->execute();
 ?>
